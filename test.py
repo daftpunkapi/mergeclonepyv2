7e24cc1eb78bdb69886eb0bd6b8858c1bfda0e72
Karan Baweja <karanbawejapro@Karans-MacBook-Pro.local>
2022-11-11 11:52:30 +0530

    third

--- test.py
+++ test.py
@@ -14,24 +14,27 @@ jira_instance = Jira(
 # Get results of jql query
 results = jira_instance.jql("project = BXB", limit = 10)
 
-
 # Load the results into a DataFrame. The list of issues is under the "issues" key of the results object.
 df = pd.json_normalize(results["issues"], errors='ignore',  max_level=None)
 
+# Add another Column "CommonStatus" i.e. Opioniated Unified Model on Jira's fields.status.name column 
+df["CommonStatus"] = [ "DONE" if x =="Done" else "NOT DONE" for x in df["fields.status.name"]]
+
 # Define which fields we care about using dot notation for nested fields.
-FIELDS_OF_INTEREST = ["id", "fields.summary", "fields.assignee.displayName", "fields.duedate", "fields.status.name", "fields.priority.name", "fields.issuetype.name", "fields.description"]
+FIELDS_OF_INTEREST = ["id", "fields.parent.id" , "fields.assignee.displayName", "fields.duedate", "fields.status.name", "CommonStatus", "fields.priority.name", "fields.issuetype.name", "fields.description"]
 
 # Filter to only display the fields we care about. To actually filter them out use df = df[FIELDS_OF_INTEREST].
 data = df[FIELDS_OF_INTEREST]
-# print(data)
+print(data)
 
-# Connect to MongoDB
+
+# # # Connect to MongoDB
 client =  MongoClient("mongodb+srv://daft:punk@mergedev.iiiixxn.mongodb.net/?retryWrites=true&w=majority")
 db = client['Ticket_Common_Model']
 collection = db['Jira']
 
-data.reset_index(inplace=True)
+# # data.reset_index(inplace=True)
 data_dict = data.to_dict("records")
 
-# Insert collection
+# # # Insert collection
 collection.insert_many(data_dict)
